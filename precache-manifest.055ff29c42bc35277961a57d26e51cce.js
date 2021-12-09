2a04302f4780f30d1de61574a5f670f27776cb47
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-09 17:45:28 +0300

    Updates

---
+++ precache-manifest.055ff29c42bc35277961a57d26e51cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253fbbce2a8d9855884b23ba71a2b629",
+    "url": "/React-ShoppingCart-Component/index.html"
+  },
+  {
+    "revision": "4bc32fe7f9c5c49206fb",
+    "url": "/React-ShoppingCart-Component/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/React-ShoppingCart-Component/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ShoppingCart-Component/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc32fe7f9c5c49206fb",
+    "url": "/React-ShoppingCart-Component/static/js/main.8eb8c3ad.chunk.js"
+  },
+  {
+    "revision": "0e75932b337bf777b0e3",
+    "url": "/React-ShoppingCart-Component/static/js/runtime-main.6e635373.js"
+  }
+]);
